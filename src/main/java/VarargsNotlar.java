57e42fa8ae39cbb70cd46100f20e58b2d43c0f95
ekimaycicek <ekimaycicek@icloud.com>
2023-06-28 15:53:24 +0300

    new

---
+++ src/main/java/VarargsNotlar.java
@@ -0,0 +1,17 @@
+public class VarargsNotlar {
+     /*
+        VARARGS :
+        var----variety cesitlilik args---arguments
+        SYNTAX ->
+        mathodName(parametre1,parametre2, dataType... dataName) {
+        meyhod budy....
+        }
+
+	        1) Method parametre parantezi icinde birden çok parametre varsa, varargs daima en sona tanımlanmalıdır .CISS
+		    2) Method parametre parantezi icinde 1'den fazla varargs ifade TANIM-LA-NA-MAZZZ :CISS
+
+           Gunun TRICK'i  köşesinde böyün :
+           Method'da  argument sayisi sabitse standart normal  method tanımlanmalı..
+           ancak argument sayisi belirli değilse  VARARGS ifade tercih edilmeli
+         */
+}
