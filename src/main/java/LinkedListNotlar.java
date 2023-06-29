ded36bf4b186800bd592cc2883ac7e5056ca03cc
ekimaycicek <ekimaycicek@icloud.com>
2023-06-30 00:47:23 +0300

    new

---
+++ src/main/java/LinkedListNotlar.java
@@ -0,0 +1,13 @@
+public class LinkedListNotlar {
+     /*
+        LinkedList .....
+        1- LinkedList'de elemanlar "data" ve "adress" olmak uzere iki ksımdan oluşur ve eleman yerine "node" olarak tanımlanır.
+        2- LinkedList'te ilk node data barındırmayan sadece adres bulunduran head node tanımlanmıştır.
+        3- LinkedList'te son node adres kısmı null olan sadece data bulunduran tail node tanımlanmıştır.
+        4- ArrayList searching LinkedList node ekleme ve silmede daha avantıjlıdır.Linkedlist eleman bulmada index yapısı olmadıgı için başarısıdır.
+        5-LİnkedLİst -> tren vagon ilşikisi baglantısı gibi düşümülebilir
+        6- Collection obj tanımlanırken cons Class(LinkedList,ArrayList...) olmalı interface(List, Queue ...) olmamasına diakkat edilmeli
+            Ancak obj DataType olarak parent interface tanımlanabilir.
+        7- LinkedList clss iki tane parent interface'den(List,Queue) implemet etmiştir.
+         */
+}
